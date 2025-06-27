8e6d90f9d7fda488c87e58310d3bb54b1a50ef52
Vick K <vickoch20@gmail.com>
2025-06-27 12:04:43 +0300

    Implement dashboard, customers, invoices pages and navigation updates

---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+// This file will be used to manage and export font configurations for the app. 
+
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+}); 
